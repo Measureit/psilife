5222539bf3fb546882f528f5123c6e91b218add2
jkurdzieko <jaroslaw.kurdzieko@gmail.com>
2025-09-03 23:04:29 +0200

    fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 // @ts-nocheck
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { BASE_URL } from './src/config/app.config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `${BASE_URL}/`, // Base URL dla aplikacji - używa stałej!
+  base: `/`, // Base URL dla aplikacji - używa stałej!
   plugins: [
     react()
   ],
